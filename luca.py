50589a352ec8b73d498a62b948fa4fbbca797533
Bob Brown <rb3@tutanota.com>
2018-07-05 08:43:37 -0400

    Initial Commit

---
+++ luca.py
@@ -0,0 +1,9 @@
+'''
+Python script to compare Census Address List with Charlottesville's 
+Master Address Table
+'''
+import csv
+
+fldr = r'C:\Users\brownr\Desktop\db\LUCA\csv'
+
+def read_csv(input_csv):
